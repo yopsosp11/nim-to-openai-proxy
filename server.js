5e377457b936f53c0b101ca83bfd9c5e40def252
yopsosp11 <shellyanxny@gmail.com>
2026-09-09 16:45:41 +1200

    Add glm-5.3 model alias to server.js

--- server.js
+++ server.js
@@ -87,6 +87,7 @@ const MODEL_MAPPING = {
   'google-lightest': 'meta/muse-glimmer-30b', // was google/gemma-2b
   'google-lighter': 'poolside/laguna-xs-2.1', // was google/gemma-3-4b-it
   'm3': 'minimaxai/minimax-m3'
+  'glm-5.3': 'z-ai/glm-5.3'
 };
 
 // Used when an unrecognized alias is requested. Must point at a live model.
